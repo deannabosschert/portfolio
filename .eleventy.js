e845d765aa4c97e6ba5f3f58e50bcae208d7c33c
“deannabosschert” <deanna.bosschert@gmail.com>
2021-01-05 19:53:27 +0100

    feat: dynamically render images

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,26 @@
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('src/assets/fonts')
   eleventyConfig.addPassthroughCopy('src/assets/img')
+  eleventyConfig.addPassthroughCopy('src/assets/img/random')
+
   // eleventyConfig.addPassthroughCopy('src/index.js')
+
+  // src for below code: https://github.com/11ty/eleventy/issues/440
+  // Import fast-glob package
+const fg = require('fast-glob')
+
+const randomIllustrations = fg.sync(['**/random/*', '!/_site'])
+const dnataIllustrations = fg.sync(['**/workplace_illustrations/*', '!**/_site'])
+
+eleventyConfig.addCollection('random', function(collection) {
+  return randomIllustrations
+})
+
+  eleventyConfig.addCollection('workplace', function(collection) {
+    return dnataIllustrations
+  })
+
+
 
   return {
     dir: {
