d7ea092f38fd9d43445075d94f2f8fd976b2b0fc
deannabosschert <deanna.bosschert@gmail.com>
2022-05-06 00:29:51 +0200

    refactor: add code comments

--- assets/js/charts/bubbles-graph.js
+++ assets/js/charts/bubbles-graph.js
@@ -1,13 +1,12 @@
 // source: https://d3-graph-gallery.com/graph/circularpacking_group.html
-// set the dimensions and margins of the graph
-var width = 900
-var height = 900
-var group1 = width / 4
-var group2 = (width / 4) * 2
-var group3 = (width / 4) * 3
+const width = 900 // set the width of the svg
+const height = 900  // set the height of the svg
+const htmlG = width / 4 // placement of the first group of circles
+const cssG = (width / 4) * 2 // placement of the second group of circles
+const jsG = (width / 4) * 3 // placement of the third group of circles
 
-// create dummy data -> just one element per circle
-var data = [{
+// create data; this is the data that will be used to create the circles. One element per circle.
+const data = [{
         "name": "HTML",
         "group": 1
     }, {
@@ -53,35 +52,38 @@ var data = [{
 
 ]
 
-// append the svg object to the body of the page
-var svg = d3.select("#bubbles_graph")
-
-
-
-// A scale that gives a X target position for each group
-var x = d3.scaleOrdinal()
+// SET SCALES
+// set an ordinal scale and set/divide (in)to 3 groups
+const x = d3.scaleOrdinal()
     .domain([1, 2, 3])
-    .range([group1, group2, group3])
+    .range([htmlG, cssG, jsG]) // place the groups in the svg
 
-// A color scale
-var color = d3.scaleOrdinal()
-    .domain([1, 2, 3])
-    .range(["#9e9effff", "#ca97caff", "#ff9dabff"])
+// set the color scale
+const color = d3.scaleOrdinal()
+    .domain([1, 2, 3]) // applyto the 3 groups
+    .range(["#9e9effff", "#ca97caff", "#ff9dabff"]) 
 
-// Initialize the circle: all located at the center of the svg area
-var circlesAll = svg.append("g") // group for the circles
-    .attr("class", "circles-all") // 
+// CREATE CONTAINERS AND ELEMENTS
+// create the svg container
+const svg = d3.select("#bubble_graph")
+    .append("svg")
+    .attr("class", "bubble_graph_svg") // give it a class
+    .attr("width", width)
+    .attr("height", height)
+
+// append the container group for all circles to the svg
+const circlesAll = svg.append("g") // group for the circleContainers
+    .attr("class", "circles-all") // class for the group/container
     .style("fill", "#202020") // set the color
     .style("fill-opacity", 0.8) // set the opacity
     .selectAll("circle") // select all circles
-    .data(data) // associate the data to the elements to add
-    .enter() // create new elements if needed
+    .data(data) // bind the data to the circles
+    .enter() // enter the data as elements
 
-
-
-var circleContainer = circlesAll
+// add a circle container element for each data element and apply drag behavior
+const circleContainer = circlesAll
     .append("g") // append a container for each data element
-    .attr("class", "circle-container")
+    .attr("class", "circle-container") // class for the group/container
     .attr("cx", width / 2) // set the x position
     .attr("cy", height / 2) // set the y position
     .call(d3.drag() // call specific function when circle is dragged
@@ -89,52 +91,56 @@ var circleContainer = circlesAll
         .on("drag", currentDrag) // while dragging
         .on("end", endDrag)) // on end of drag gesture
 
-var circleContainer__circle = circleContainer
+// add the circle to the circleContainer(s)
+const circleContainer__circle = circleContainer 
     .append("circle") // append a circle for each data element
-    .attr("r", 50) // set the radius
     .attr("class", "circle-container__circle")
+    .attr("r", 50) // set the radius
+    .attr("stroke", "#595959b3") // set the outline color
+    .attr("stroke-width", 0.8) // set the outline width
     .style("fill", (d) => color(d.group)) // set the color
     .style("fill-opacity", 0.8) // set the opacity
-    .attr("stroke", "#595959b3") // set the outline color
-    .style("stroke-width", 1) // set the outline width
 
-// add the text to the nodes
-var circleContainer__text = circleContainer
-    .append("text")
+// add the text to the circleContainer(s)
+const circleContainer__text = circleContainer
+    .append("text") // append text for each data element
     .attr("class", "circle-container__text") // give each dot a class called "circle-container__text"
-    .text((d) => d.name)
-    .attr('dx', (d) => x(d.group))
-    .attr('dy', (d) => height / 2)
+    .text((d) => d.name) // set the text to the name of the data element
+    .attr('dx', (d) => x(d.group)) // set the x position of the text to the x position of the circleContainer
+    .attr('dy', (d) => height / 2) // set the y position of the text to the center of the circleContainer
     .attr('font-size', (d) => '24px')
     .attr('fill', (d) => '#595959b3')
-    .attr('text-anchor', (d) => 'middle')
-    .attr('alignment-baseline', (d) => 'central')
+    .attr('text-anchor', (d) => 'middle') // center horizontally
+    .attr('alignment-baseline', (d) => 'central') // center vertically
     .attr('font-weight', (d) => 'bold')
 
 
-// Features of the forces applied to the nodes:
-var simulation = d3.forceSimulation()
-    .force("x", d3.forceX().strength(0.5).x((d) => x(d.group)))
-    .force("y", d3.forceY().strength(0.1).y(height / 2))
-    .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
-    .force("charge", d3.forceManyBody().strength(1)) // Nodes are attracted one each other of value is > 0
-    .force("collide", d3.forceCollide().strength(.1).radius(32).iterations(1)) // Force that avoids circle overlapping
+// CREATE FORCE LAYOUT
+// create simulation that will be used to move the circles
+const simulation = d3.forceSimulation() // create the force simulation
+    .force("x", d3.forceX().strength(0.5).x((d) => x(d.group))) // set force to move circles horizontally
+    .force("y", d3.forceY().strength(0.1).y(height / 2)) // set force to move circles vertically
+    .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // set force to center the circles
+    .force("charge", d3.forceManyBody().strength(1)) // set force to repel circles from each other
+    .force("collide", d3.forceCollide().strength(.1).radius(32).iterations(1)) // set force to prevent circles from overlapping
 
-// Apply these forces to the nodes and update their positions.
-// Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
+
+// apply the simulation to the circles, this will make the circles move
+// the simulation will run for a number of iterations
+// the simulation will stop when the simulation.alpha() is less than 0.01
 simulation
     .nodes(data)
     .on("tick", ticked);
 
-function ticked() { // this forces the circles to stay in the center of the svg area
-    // update circle positions each tick of the force simulation, then update the circle positions into groups
+// this is called each time the simulation ticks (which is at each iteration)
+function ticked() { 
     circleContainer__circle // select the elements
-        .attr("cx", (d) => d.x) // set the x position to the current position of the node in the force layout simulation (which is the x position of the node in the svg)
+        .attr("cx", (d) => d.x) // set the x position to the current position of the node in the force layout simulation (which is the x position of the circleContainer in the svg)
         .attr("cy", (d) => d.y) // set the y position to the current y position
 
-    circleContainer__text // select the elements
-        .attr("dx", (d) => d.x) // set the x position to the current position of the node in the force layout simulation (which is the x position of the node in the svg)
-        .attr("dy", (d) => d.y) // set the y position to the current y position
+    circleContainer__text // select text in each circle
+        .attr("dx", (d) => d.x) // dx is the x position of the text
+        .attr("dy", (d) => d.y) // dy is the y position of the text
 }
 
 function startDrag(d) { // when a circle is dragged
