54615dbfe7193ea736a6182a07ce5663ec58a27a
deannabosschert <deanna.bosschert@gmail.com>
2022-05-09 16:11:44 +0200

    feat: create graph with project data

--- assets/js/charts/bubbles-graph.js
+++ assets/js/charts/bubbles-graph.js
@@ -7,61 +7,12 @@ const height = 900 // set the height of the svg
 const htmlG = width / 4 // placement of the first group of circles
 const cssG = (width / 4) * 2 // placement of the second group of circles
 const jsG = (width / 4) * 3 // placement of the third group of circles
+// clean the data; this is the data that will be used to create the circles. One element per circle.
 
-// create data; this is the data that will be used to create the circles. One element per circle.
-const data = [{
-        "name": "HTML",
-        "group": 1
-    }, {
-        "name": "A11y",
-        "group": 1
-    }, {
-        "name": "Dingen",
-        "group": 1
-    }, {
-        "name": "CSS",
-        "group": 2
-    }, {
-        "name": "SASS",
-        "group": 2
-    }, {
-        "name": "SCSS",
-        "group": 2
-    }, {
-        "name": "InuitCSS",
-        "group": 2
-    }, {
-        "name": "JavaScript",
-        "group": 3
-    }, {
-        "name": "Node.js",
-        "group": 3
-    }, {
-        "name": "Eleventy",
-        "group": 3
-    }, {
-        "name": "Express",
-        "group": 3
-    }, {
-        "name": "Databases",
-        "group": 3
-    }, {
-        "name": "EJS",
-        "group": 3
-    }, {
-        "name": "Vue.js",
-        "group": 3
-    }
-
-]
-
-// get function projectData from file projects.js
 import projectData from '/assets/js/storyblok/JS/projects.js'
 const techData = projectData.map(project => project.technologies)
 const techTypes = [...new Set(techData.map(tech => Object.keys(tech)).flat())]
-
-const cleanTechData = forEachType(techTypes, technologiesToObject).flat()
-
+const data = forEachType(techTypes, technologiesToObject).flat()
 
 function forEachType(array) {
     let resultArray = []
@@ -72,13 +23,12 @@ function forEachType(array) {
     return resultArray
 }
 
-
-
 function technologiesToObject(tech, groupNumber) {
     const techArray = mapTech(techData, tech)
     const techNames = reduceArray(techArray).filter(item => item)
     return techToObject(techNames, groupNumber)
 }
+
 function mapTech(data, tech) {
     return data.map(data => data[tech])
 }
@@ -88,7 +38,7 @@ function reduceArray(data) {
 }
 
 function techToObject(array, groupNumber) {
-   return array.map(tech => {
+    return array.map(tech => {
         return {
             name: tech,
             group: groupNumber
@@ -102,12 +52,10 @@ function techToObject(array, groupNumber) {
 const x = d3.scaleOrdinal()
     .domain([1, 2, 3])
     .range([htmlG, cssG, jsG]) // place the groups in the svg
-
 // set the color scale
 const color = d3.scaleOrdinal()
     .domain([1, 2, 3]) // applyto the 3 groups
     .range(["#ca97caff", "#ffb6c1ff", "#9e9effff"])
-
 // CREATE CONTAINERS AND ELEMENTS
 // create the svg container
 const svg = d3.select("#bubble_graph")
@@ -115,14 +63,12 @@ const svg = d3.select("#bubble_graph")
     .attr("class", "bubble_graph_svg") // give it a class
     .attr("width", width)
     .attr("height", height)
-
 // append the container group for all circles to the svg
 const circlesAll = svg.append("g") // group for the circleContainers
     .attr("class", "circles-all") // class for the group/container
     .selectAll("circle") // select all circles
     .data(data) // bind the data to the circles
     .enter() // enter the data as elements
-
 // add a circle container element for each data element and apply drag behavior
 const circleContainer = circlesAll
     .append("g") // append a container for each data element
@@ -134,7 +80,6 @@ const circleContainer = circlesAll
         .on("start", startDrag) // on start of drag gesture
         .on("drag", currentDrag) // while dragging
         .on("end", endDrag)) // on end of drag gesture
-
 // add the circle to the circleContainer(s)
 const circleContainer__circle = circleContainer
     .append("circle") // append a circle for each data element
@@ -144,7 +89,6 @@ const circleContainer__circle = circleContainer
     .attr("stroke-width", 0.8) // set the outline width
     .style("fill", (d) => color(d.group)) // set the color
     .style("fill-opacity", 0.8) // set the opacity
-
 // add the text to the circleContainer(s)
 const circleContainer__text = circleContainer
     .append("text") // append text for each data element
@@ -159,8 +103,6 @@ const circleContainer__text = circleContainer
     .attr('alignment-baseline', (d) => 'central') // center vertically
     .attr('font-weight', (d) => 'bold')
     .style('user-select', 'none') // don't let the text be selectable
-
-
 // CREATE FORCE LAYOUT
 // create simulation that will be used to move the circles
 const simulation = d3.forceSimulation() // create the force simulation
@@ -169,26 +111,21 @@ const simulation = d3.forceSimulation() // create the force simulation
     .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // set force to center the circles
     .force("charge", d3.forceManyBody().strength(1)) // set force to repel circles from each other
     .force("collide", d3.forceCollide().strength(.1).radius(60).iterations(1)) // set force to prevent circles from overlapping
-
-
 // apply the simulation to the circles, this will make the circles move
 // the simulation will run for a number of iterations
 // the simulation will stop when the simulation.alpha() is less than 0.01
 simulation
     .nodes(data)
     .on("tick", ticked);
-
 // this is called each time the simulation ticks (which is at each iteration)
 function ticked() {
     circleContainer__circle // select the elements
         .attr("cx", (d) => d.x) // set the x position to the current position of the node in the force layout simulation (which is the x position of the circleContainer in the svg)
         .attr("cy", (d) => d.y) // set the y position to the current y position
-
     circleContainer__text // select text in each circle
         .attr("dx", (d) => d.x) // dx is the x position of the text
         .attr("dy", (d) => d.y) // dy is the y position of the text
 }
-
 // CREATE DRAG FUNCTIONALITY
 function startDrag(d) { // when a circle is dragged
     if (!d3.event.active) simulation.alphaTarget(.03).restart() // if the simulation isn't running, start it
